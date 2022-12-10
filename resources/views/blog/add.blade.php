4a29632196c93787359851745868f780396e463f
nishamedayannur <nishamedayannur@gmail.com>
2022-12-10 09:50:08 +0530

    Update add.blade.php

--- resources/views/blog/add.blade.php
+++ resources/views/blog/add.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
-                <a class="nav-link" href="{{ url('category') }}">{{ __('Add Blog') }}</a>
+                <a class="nav-link" >{{ __('Add Blog') }}</a>
                 </div>
 
                 <div class="card-body">
